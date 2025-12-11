c5f1aa172b1acb3da8f5f8b14b742acd0f40562d
Darien-Cepani <60065437+Darien-Cepani@users.noreply.github.com>
2025-12-11 11:54:13 +0100

    Challenge 3&4: Fixed linting issues and Dockerfile build process.

--- bookstore/routes.py
+++ bookstore/routes.py
@@ -2,16 +2,17 @@ import json
 from flask import jsonify, request, abort
 from flasgger import Swagger
 from bookstore import app
-
 swagger = Swagger(app)
 
 def load_books():
-    with open('bookstore/books.json', 'r') as file:
+    """Load and return the list of books from the JSON data file."""
+    with open('bookstore/books.json', 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
-def save_books(books):
-    with open('bookstore/books.json', 'w') as file:
+def save_books(books)
+    """Save the list of books to the JSON data file."""
+    with open('bookstore/books.json', 'w', encoding='utf-8') as file:
         json.dump(books, file, indent=4)
 
 
@@ -75,7 +76,7 @@ def add_book(book_id):
     if any(book['id'] == book_id for book in books):
         abort(400, description="Book ID already exists.")
     book_data = request.json
-    testing_variable = book_data['name']
+    # testing_variable = book_data['name']
     required_fields = ['name', 'author', 'isbn', 'price']
     if not all(field in book_data for field in required_fields):
         abort(400, description="Missing required fields.")
@@ -119,7 +120,6 @@ def update_book(book_id):
     book = next((book for book in books if book['id'] == book_id), None)
     if book is None:
         abort(404, description="Book not found.")
-        
     book['price'] = request.json.get('price', book['price'])
     save_books(books)
     return jsonify(book), 201
@@ -142,7 +142,7 @@ def delete_book(book_id):
         description: Book not found
     """
     books = load_books()
-    book = next((book for book in books if book['id'] == Book_id), None)
+    book = next((book for book in books if book['id'] == book_id), None)
     if book is None:
         abort(404, description="Book not found.")
     books = [book for book in books if book['id'] != book_id]
